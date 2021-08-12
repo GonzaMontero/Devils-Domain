0164ecbd4bb0462102ee3fb02732883d31243f13
alejandroninsausti <alejandroninsausti@gmail.com>
2021-08-12 18:38:30 -0300

    Add CellSelector

---
+++ DSVJ3_GAME/Assets/Scripts/CellSelector.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Cell { }
+
+public static class CellSelector
+{
+    public static Cell GetCellOnMousePosition()
+    {
+        RaycastHit cellHitted;
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
+        if(Physics.Raycast(cameraRay, out cellHitted, 100, LayerMask.GetMask("Cells")))
+        {
+            return cellHitted.transform.GetComponent<Cell>();
+        }
+
+        return null;
+    }
+}
