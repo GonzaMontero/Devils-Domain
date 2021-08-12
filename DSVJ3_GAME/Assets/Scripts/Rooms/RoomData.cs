1123c1d71000ae57b44d1d573fdad3a3fd4e7c53
alejandroninsausti <alejandroninsausti@gmail.com>
2021-08-12 19:54:29 -0300

    Add RoomData & SO

---
+++ DSVJ3_GAME/Assets/Scripts/Rooms/RoomData.cs
@@ -0,0 +1,13 @@
+﻿[System.Serializable]
+public class RoomData
+{
+    public RoomSO so;
+    public int goldGen;
+    public int upgradeLvl;
+
+    public void SetCurrents()
+    {
+        goldGen = so.baseGoldGeneration;
+        upgradeLvl = 1;
+    }
+}
