f38cdb4c1af1dc5dd41b3bd7e3b73a1093b3891f
alejandroninsausti <alejandroninsausti@gmail.com>
2021-08-18 10:12:15 -0300

    Add RoomController

---
+++ DSVJ3_GAME/Assets/Scripts/Rooms/RoomController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+	public Action RoomDestroy;
+	public Action RoomUpdate;
+	[SerializeField] RoomData data;
+	int[] roomLimits = new int[4];
+
+    public void Build(List<Tile> roomTiles, RoomSO so)
+	{
+        data.so = so;
+
+        foreach (Tile tile in roomTiles)
+        {
+            //Link Action
+            //RoomIsBeingDestroyed += tile.OnRoomBeingDestroyed;
+            
+            //Set Room Limits       OPTIMIZE
+            if (tile.X < roomLimits[0])
+            {
+                roomLimits[0] = tile.X;
+            }
+            if (tile.X > roomLimits[1])
+            {
+                roomLimits[1] = tile.X;
+            }
+            if (tile.Y < roomLimits[2])
+            {
+                roomLimits[2] = tile.Y;
+            }
+            if (tile.Y > roomLimits[3])
+            {
+                roomLimits[3] = tile.Y;
+            }
+        }
+	}
+	public void Destroy()
+    {
+		RoomDestroy?.Invoke();
+        Destroy(gameObject);
+    }
+    public float GetUpgradeCost()
+    {
+        return Mathf.Pow(data.so.baseCost * data.so.updgradeCostMod, data.upgradeLvl);
+    }
+	public void Upgrade()
+    {
+        RoomUpdate.Invoke();
+        data.upgradeLvl++;
+        data.goldGen *= data.so.baseGoldGeneration;
+    }
+	public int GetGoldGen()
+    {
+        return data.goldGen;
+    }
+}
