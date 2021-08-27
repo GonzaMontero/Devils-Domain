9918df85275cad118b6de806cda9b63ab5fe0f47
Gonzalo Montero <gonmon654@gmail.com>
2021-08-27 19:11:58 -0300

    Added Battle Scene and Character FSM

---
+++ DSVJ3_GAME/Assets/Scripts/CharacterFSM.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Animations;
+
+[RequireComponent(typeof(Animator))]
+public class CharacterFSM : MonoBehaviour
+{
+    enum States {idle,attacking,dead };
+    States current;
+
+    void Start()
+    {
+        current = States.idle;
+    }
+
+    void Update()
+    {
+        switch (current)
+        {
+            case States.idle:                
+                break;
+            case States.attacking:
+                break;
+            case States.dead:
+                break;
+        }
+    }
+
+    IEnumerator SwitchIdleAttack()
+    {
+        yield return null;
+    }
+}
