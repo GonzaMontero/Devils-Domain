e9eb7bedf0984945397e271536abea8d29d7c87a
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2021-08-30 05:01:08 -0300

    Add BattleManager

---
+++ DSVJ3_GAME/Assets/Scripts/BattleManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    BattleCharacterController[] characters = new BattleCharacterController[12]; //first 6 are player
+    Tile[] characterTiles = new Tile[12]; //second 6 are enemies (same as player)
+    //slots are organized in proximity, so matrix would be
+    // 1 0 - 6 7
+    // 3 2 - 8 9
+    // 4 5 - 10 11
+
+    #region Unity Events
+    private void Start()
+    {
+        //Get Characters
+
+        //Link
+        foreach (BattleCharacterController character in characters)
+        {
+            character.Attack += OnCharacterAttack;
+        }
+    }
+    #endregion
+
+    #region Methods
+    void OnCharacterAttack(BattleCharacterController attacker)
+    {
+        int receiverNumber = attacker.slotNumber;
+        receiverNumber += attacker.slotNumber >= 6 ? -6 : 6; //get parallel
+
+        //check if distance is correct and attack
+        switch (attacker.GetAttackType())
+        {
+            case AttackType.melee:
+                if (receiverNumber % 2 != 0) { return; } //only attack front liners
+                characters[receiverNumber]?.ReceiveDamage(attacker.GetAttackDamage());
+                break;
+            case AttackType.ranged:
+                characters[receiverNumber]?.ReceiveDamage(attacker.GetAttackDamage());
+                break;
+            case AttackType.magic:
+                if (receiverNumber % 2 == 0) { return; } //only attack back liners
+                characters[receiverNumber]?.ReceiveDamage(attacker.GetAttackDamage());
+                break;
+            default:
+                break;
+        }
+    }
+    #endregion
+}
