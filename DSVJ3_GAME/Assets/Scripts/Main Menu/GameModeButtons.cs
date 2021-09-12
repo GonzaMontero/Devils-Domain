2008c8931b4c68cc23770ed36d1d1b540084c73b
Gonzalo Montero <gonmon654@gmail.com>
2021-09-12 18:12:26 -0300

    Finished Main Menu Buttons and Details

---
+++ DSVJ3_GAME/Assets/Scripts/Main Menu/GameModeButtons.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameModeButtons : MonoBehaviour
+{
+    [SerializeField] GameObject underMainteinanceBanner;
+    [SerializeField] GameObject mainMenuBanner;
+
+    public void GoToAdventure()
+    {
+        SceneManager.LoadScene("BattleTest");
+    }
+    public void GoToUnderManteinance()
+    {
+        mainMenuBanner.SetActive(false);
+        underMainteinanceBanner.SetActive(true);
+    }
+    public void GoToMainMenu()
+    {
+        underMainteinanceBanner.SetActive(false);
+        mainMenuBanner.SetActive(true);
+    }
+    public void GoToIdle()
+    {
+        SceneManager.LoadScene("ProtoRoom");
+    }
+}
