3d1278a3b25d6c7b5a0a9b9b5f5568a49ba75e6e
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2021-10-06 10:47:08 -0300

    Add Gem and Gold Counter

--- DSVJ3_GAME/Assets/Scripts/Main Menu/UIPlayerData.cs
+++ DSVJ3_GAME/Assets/Scripts/Main Menu/UIPlayerData.cs
@@ -1,16 +1,29 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class UIPlayerData : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI goldText;
     [SerializeField] TextMeshProUGUI gemsText;
-    [SerializeField] TextMeshProUGUI nameText;
 	Player player;
+    string sceneName;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        //Scene
+        sceneName = SceneManager.GetActiveScene().name;
+        SceneManager.activeSceneChanged += OnSceneChange;
+    }
+
+    
+    void OnSceneChange(Scene oldScene, Scene newScene)
+    {
+        if (newScene.name != sceneName)
+        {
+            goldText.text = player.gold.ToString();
+            gemsText.text = player.gems.ToString();
+        }
     }
 }
