1a4dfb48e93cfaa3ece0b80e1670dc465bc72400
Gonzalo Montero <gonmon654@gmail.com>
2021-11-09 19:00:00 -0300

    Modified Lineup Excess Characters

---
+++ DSVJ3_GAME/Assets/Scripts/Lineup/LoadExcessCharacters.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadExcessCharacters : MonoBehaviour
+{
+    [SerializeField] GameObject[] excessImages;
+    [SerializeField] Sprite emptyFace;
+
+    Player player;
+
+    private void Start()
+    {
+        player = Player.Get();
+
+        for(short i = 0; i < excessImages.Length; i++)
+        {
+            if (player.characters[i].so != null)
+            {
+                excessImages[i].GetComponent<Image>().sprite = player.characters[i].so.lineupFaceSprite;
+            }
+            else if(player.characters.Count < i)
+            {
+                excessImages[i].GetComponent<Image>().sprite = emptyFace;
+            }
+            else
+            {
+                excessImages[i].GetComponent<Image>().sprite = emptyFace;
+            }
+        }
+    }
+}
