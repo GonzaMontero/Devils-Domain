6d2b7f705814b831d082475b547d02c9b94c0beb
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2021-11-10 09:52:02 -0300

    Add GameManager

---
+++ DSVJ3_GAME/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviourSingleton<GameManager>
+{
+
+    #region Unity Events
+
+    #endregion
+
+    public void LoadMenu()
+    {
+        SceneLoader.LoadMenu();
+    }
+    public void LoadAutobattle()
+    {
+        SceneLoader.LoadAutobattle();
+    }
+    public void LoadIdle()
+    {
+        SceneLoader.LoadIdle();
+    }
+    public void LoadGacha()
+    {
+        SceneLoader.LoadGacha();
+    }
+    public void LoadLineup()
+    {
+        SceneLoader.LoadLineup();
+    }
+}
---
+++ DSVJ3_GAME/Assets/Scripts/Main Menu/UIMainMenuManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class UIMainMenuManager : MonoBehaviour
+{
+    public Action AutoBattleButtonPressed;
+    public Action IdleButtonPressed;
+    public Action LineupButtonPressed;
+    public Action GachaButtonPressed;
+    [SerializeField] GameObject underMainteinanceBanner;
+    [SerializeField] GameObject mainMenuBanner;
+    GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = GameManager.Get();
+        AutoBattleButtonPressed += gameManager.LoadAutobattle;
+        IdleButtonPressed += gameManager.LoadIdle;
+        LineupButtonPressed += gameManager.LoadLineup;
+        GachaButtonPressed += gameManager.LoadGacha;
+    }
+    public void GoToAdventure()
+    {
+        AutoBattleButtonPressed.Invoke();
+    }
+    public void GoToIdle()
+    {
+        IdleButtonPressed.Invoke();
+    }
+    public void GoToLineup()
+    {
+        LineupButtonPressed.Invoke();
+    }
+    public void GoToGacha()
+    {
+        GachaButtonPressed.Invoke();
+    }
+}
