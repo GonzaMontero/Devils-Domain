2db005989824f603e7ecddcac4d45313d03f1eb1
Gonzalo Montero <gonmon654@gmail.com>
2021-11-10 17:22:10 -0300

    Added base for level Setup

---
+++ DSVJ3_GAME/Assets/Scripts/Autobattle/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] List<BattleCharacterSO[]> levels;
+
+    [SerializeField] GameObject[] enemySlots;
+
+    private void Start()
+    {
+        
+    }
+}
