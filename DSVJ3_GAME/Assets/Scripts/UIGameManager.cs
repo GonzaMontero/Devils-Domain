9cb4e9d47f31be9fa9e8a322449779ffc61343f7
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2021-11-16 21:24:13 -0300

    Fix SceneLoading Buttons

---
+++ DSVJ3_GAME/Assets/Scripts/UIGameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class UIGameManager : MonoBehaviour
+{
+    GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = GameManager.Get();
+    }
+
+    public void LoadMenu()
+    {
+        gameManager.LoadMenu();
+    }
+    public void LoadAutobattle()
+    {
+        gameManager.LoadAutobattle();
+    }
+    public void LoadIdle()
+    {
+        gameManager.LoadIdle();
+    }
+    public void LoadGacha()
+    {
+        gameManager.LoadGacha();
+    }
+    public void LoadLineup()
+    {
+        gameManager.LoadLineup();
+    }
+}
