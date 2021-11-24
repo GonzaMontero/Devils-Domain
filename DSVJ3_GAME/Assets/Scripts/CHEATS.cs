62384f3d75cc7c4350b9872f806467b86fd82ef9
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2021-11-24 17:08:27 -0300

    Add gold & add gems inspector cheats

--- DSVJ3_GAME/Assets/Scripts/CHEATS.cs
+++ DSVJ3_GAME/Assets/Scripts/CHEATS.cs
@@ -4,15 +4,27 @@ public class CHEATS : MonoBehaviour
 {
     [SerializeField] Player player;
 
+    #region Editor Things
 #if UNITY_EDITOR//||true
+    
+    //Character Set Level 1
     [Tooltip("Press this to set the character in the lineup index to level 1")]
     [SerializeField] bool setLevel1Character = false;
     [SerializeField] int level1CharacterLineupIndex;
 
+    //Add Gold
+    [Tooltip("Press this to add as much gold as indicated")]
+    [SerializeField] bool addGold = false;
+    [SerializeField] int addGoldQuantity;
+
+    [Tooltip("Press this to add as much gems as indicated")]
+    [SerializeField] bool addGems = false;
+    [SerializeField] int addGemsQuantity;
+
     //Unity Events
-    private void OnValidate()
+    private void OnValidate() //You can have multiple booleans here
     {
-        //You can have multiple booleans here
+        //SET CHARACTER LEVEL 1
         if (setLevel1Character)
         {
             // Your function here
@@ -21,6 +33,31 @@ public class CHEATS : MonoBehaviour
             //When its done set this bool to false
             //This is useful if you want to do some stuff only when clicking this "button"
             setLevel1Character = false;
+            return;
+        }
+
+        //ADD X GOLD
+        if (addGold)
+        {
+            // Your function here
+            AddGold(addGoldQuantity);
+
+            //When its done set this bool to false
+            //This is useful if you want to do some stuff only when clicking this "button"
+            addGold = false;
+            return;
+        }
+
+        //ADD X GEMS
+        if (addGems)
+        {
+            // Your function here
+            AddGems(addGemsQuantity);
+
+            //When its done set this bool to false
+            //This is useful if you want to do some stuff only when clicking this "button"
+            addGems = false;
+            return;
         }
     }
 
@@ -32,4 +69,14 @@ public class CHEATS : MonoBehaviour
         return new BattleCharacterData(characterToSet.so);
     }
 #endif
+    #endregion
+
+    public void AddGold(int gold)
+    {
+        player.gold += gold;
+    }
+    public void AddGems(int gems)
+    {
+        player.gems += gems;
+    }
 }
