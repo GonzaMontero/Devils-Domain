40396b307d9c420c86c50807f4828a123f043fb1
Gonzalo Montero <gonmon654@gmail.com>
2021-11-26 16:50:07 -0300

    Modified Lineup To Display Character Health and Damage

--- DSVJ3_GAME/Assets/Scripts/Lineup/LoadTeam.cs
+++ DSVJ3_GAME/Assets/Scripts/Lineup/LoadTeam.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LoadTeam : MonoBehaviour
 {
     [SerializeField] GameObject[] lineupFaceImage;
+    [SerializeField] TextMeshProUGUI[] lineupDetailsText;
     [SerializeField] Player player;
     [SerializeField] Sprite emptyPlayerSprite;
 
@@ -15,11 +17,15 @@ public class LoadTeam : MonoBehaviour
 
     private void Start()
     {
+        int totalHealth = 0;
+        int totalDamage = 0;
         for(short i = 0; i < player.lineup.Length; i++)
         {
             if (player.lineup[i].so != null)
             {
                 lineupFaceImage[i].GetComponent<Image>().sprite = player.lineup[i].so.lineupFaceSprite;
+                totalHealth += player.lineup[i].currentStats.maxHealth;
+                totalDamage += player.lineup[i].currentStats.damage;
             }
             else
             {
@@ -27,5 +33,31 @@ public class LoadTeam : MonoBehaviour
             }
             lineupFaceImage[i].GetComponentInParent<StartSwap>().slotOnArray = i;
         }
+       
+        lineupDetailsText[0].text = "Damage: " + totalDamage;
+        lineupDetailsText[1].text = "Health: " + totalHealth;
+    }
+
+    private void OnEnable()
+    {
+        int totalHealth = 0;
+        int totalDamage = 0;
+        for (short i = 0; i < player.lineup.Length; i++)
+        {
+            if (player.lineup[i].so != null)
+            {
+                lineupFaceImage[i].GetComponent<Image>().sprite = player.lineup[i].so.lineupFaceSprite;
+                totalHealth += player.lineup[i].currentStats.maxHealth;
+                totalDamage += player.lineup[i].currentStats.damage;
+            }
+            else
+            {
+                lineupFaceImage[i].GetComponent<Image>().sprite = emptyPlayerSprite;
+            }
+            lineupFaceImage[i].GetComponentInParent<StartSwap>().slotOnArray = i;
+        }
+
+        lineupDetailsText[0].text = "Damage: " + totalDamage;
+        lineupDetailsText[1].text = "Health: " + totalHealth;
     }
 }
