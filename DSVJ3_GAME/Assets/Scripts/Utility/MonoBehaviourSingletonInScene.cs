b8245c0e8d89973ecda69b4a91591a9cb4ec4dcf
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2021-12-11 14:47:55 -0300

    Modify Lineup scale

---
+++ DSVJ3_GAME/Assets/Scripts/Utility/MonoBehaviourSingletonInScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MonoBehaviourSingletonInScene<T> : MonoBehaviour where T : Component
+{
+    private static T instance;
+
+    public static T Get()
+    {
+        return instance;
+    }
+
+    public virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
